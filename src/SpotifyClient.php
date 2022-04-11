f8a1ab24e7bd4633304e33f81e8b960612ad3a9a
AhmedSalheia <ahmedsalheia.as@gmail.com>
2022-04-12 01:43:10 +0300

    finsihed UserProfileClient.php

--- src/SpotifyClient.php
+++ src/SpotifyClient.php
@@ -9,7 +9,7 @@ class SpotifyClient
 {
     public static function __callStatic($name, $params)
     {
-        $class = "App\Http\Helpers\Spotify\\".ucfirst($name).'Client';
+        $class = "SpotifyClient\Spotify\\".ucfirst($name).'Client';
         if (class_exists($class))
             return $class::makeInstance();
         throw new ClassNotFoundException("Class {$class} Does Not Exist",$class);
